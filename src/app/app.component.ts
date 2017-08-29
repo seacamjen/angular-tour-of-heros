cc2d31173c1c074e054e3b518e936e29f122a570
Cameron Jensen <jensenseanc@gmail.com>
2017-08-29 16:47:04 -0700

    add mock heroes to store heros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero.model';
-
-const HEROES: Hero[] = [
-  {id: 11, name: 'Windslow'},
-  {id: 12, name: 'Barty'},
-  {id: 13, name: 'Cravens'},
-  {id: 14, name: 'Firenor'},
-  {id: 15, name: 'Dynama'},
-  {id: 16, name: 'Tornado'},
-]
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService],
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'Tour of Heros';
-  heroes = HEROES;
+  heroes: Hero [];
   selectedHero: Hero;
+
+  constructor(private heroService: HeroService) { }
 
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
-}
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from './hero.model';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Windslow'},
+  {id: 12, name: 'Barty'},
+  {id: 13, name: 'Cravens'},
+  {id: 14, name: 'Firenor'},
+  {id: 15, name: 'Dynama'},
+  {id: 16, name: 'Tornado'},
+]
