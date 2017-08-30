692049a075f31ee9da7d5531c933ffb257d828f8
Cameron Jensen <jensenseanc@gmail.com>
2017-08-30 16:59:04 -0700

    add hero serach component

--- src/app/hero-search.service.ts
+++ src/app/hero-search.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+import { Hero } from './hero.model';
 
 @Injectable()
 export class HeroSearchService {
 
-  constructor() { }
+  constructor(private http: Http) { }
 
+  search(term: string): Observable<Hero[]> {
+    return this.http
+                .get(`api/heroes/?name=${term}`)
+                .map(response => response.json().data as Hero[]);
+  }
 }
