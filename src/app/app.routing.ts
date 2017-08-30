f55134cc65f666b11da7ef10205ec49cd9ad07bf
Cameron Jensen <jensenseanc@gmail.com>
2017-08-30 13:53:36 -0700

    add routing to specific hero id

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component'
 import { DashboardComponent } from './dashboard/dashboard.component'
+import { HeroDetailComponent } from './hero-detail/hero-detail.component'
 
 const appRoutes: Routes = [
   {
@@ -16,6 +17,10 @@ const appRoutes: Routes = [
   {
     path: 'dashboard',
     component: DashboardComponent
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
   }
  ]
 
